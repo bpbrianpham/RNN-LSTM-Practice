534c63178fe84372246e0c82d1fbbf8db021e590
bpham <brianthongpham@gmail.com>
2018-04-28 22:13:09 -0700

    fixed file directory

--- Code/LSTM_for_Regression.py
+++ Code/LSTM_for_Regression.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     numpy.random.seed(7)
     
     # load the dataset
-    dataframe = read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+    dataframe = read_csv('../Data/international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
     dataset = dataframe.values
     dataset = dataset.astype('float32')
     
